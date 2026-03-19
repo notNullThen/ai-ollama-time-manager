2cae6d186d2a80ff557f37a203e933230bca8f82
notNullThen <epekaryuk@outlook.com>
2026-03-19 22:35:51 +0100

    TimeEntry AI friendly json property name

--- TimeCalculator/Core/Types/TimeEntry.cs
+++ TimeCalculator/Core/Types/TimeEntry.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace TimeCalculator.Core.Types;
 
 public class TimeEntry
 {
+    [JsonPropertyName("timeEntry")]
     public TimeData Time { get; set; } = new();
     public TimeType Type { get; set; } = TimeType.Work;
 
